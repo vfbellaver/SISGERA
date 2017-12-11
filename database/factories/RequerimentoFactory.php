6839d3ae0dd710dee6e6f5019c6a9fe553a34ff4
Vagner Bellaver <vfbellaver@gmail.com>
2017-12-11 09:42:31 -0300

    #10 criado view de PDF, ajustado factory de requerimento

--- database/factories/RequerimentoFactory.php
+++ database/factories/RequerimentoFactory.php
@@ -16,17 +16,18 @@ $factory->define(\Sisgera\Models\Requerimento::class, function (Faker $faker) {
             ];
 
     $curso = $cursos[rand(0, 3)];
+    $st = $status[rand(0, 4)];
     return [
         'data_criacao' =>\Carbon\Carbon::now()->format('Y-m-d H:m:s'),
         'protocolo' => $faker->swiftBicNumber,
         'justificativa' => $faker->text($maxNbChars = 600),
-        'situacao' => $status[rand(0, 4)],
-        'resposta' =>  'situacao'== $status[2] || 'situacao'== $status[3] || 'situacao'== $status[4] ? $faker->text($maxNbChars = 300) : null,
+        'situacao' => $st,
+        'resposta' =>  $st == $status[2] || $st == $status[3] || $st == $status[4] ? $faker->text($maxNbChars = 300) : null,
         'curso' => $curso['nome'],
         'turno' => $curso['turno'],
         'periodo' => $faker->numberBetween(0,9),
         'turma' => '1000',
-        'data_fechamento' => 'situacao'== $status[3] ?  \Carbon\Carbon::now()->addMonths(3)->format('Y-m-d d-m-Y H:m:s') : null,
+        'data_fechamento' => $st == $status[2] || $st == $status[3] || $st == $status[4] ?  \Carbon\Carbon::now()->addMonths(3)->format('Y-m-d H:m:s') : null,
 
 
     ];
